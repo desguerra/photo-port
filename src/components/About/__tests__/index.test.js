c26671f4f8f9885c6d3aceabbb6045adb7451ab6
desguerra <ddesguerra@ucdavis.edu>
2022-11-13 22:23:06 -0800

    react About tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+/* The render function will do just what its name implies: "render" 
+the component. What actually happens is that Jest creates a simulated 
+DOM in a Node.js environment to approximate the DOM (no component is 
+actually visibly rendered).
+
+The cleanup function is used to remove components from the DOM to 
+prevent memory leaking, as well as variable or data collisions between 
+tests that could corrupt test results. */
+import { render, cleanup } from '@testing-library/react';
+
+/* jest-dom offers access to custom matchers that are used to test DOM elements */
+import '@testing-library/jest-dom/extend-expect';
+
+/* import the component we will be testing */
+import About from '..';
+
+/* will ensure that after each test, we won't have any leftover memory data 
+that could give us false results. */
+afterEach(cleanup);
+
+describe('About component', () => {
+  // First Test
+  it('renders', () => {
+    render(<About />);
+  });
+
+  // Second Test
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
